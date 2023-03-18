33d9f8d74326fb42b16672c451cf4804a2d04512
23Point5 <97957440+23point5@users.noreply.github.com>
2023-03-18 21:03:32 +0800

    brand logo added

---
+++ src/BrandLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrandLogo: React.FC = () => (
+  <img
+    style={{ width: '200px', marginBottom: '1rem' }}
+    src='https://www.23point5.com/imgs/app/23Point5_Logo_Black_RGB.svg'
+    alt='23point'
+  />
+)
+
+export default BrandLogo
